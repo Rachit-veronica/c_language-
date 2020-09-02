017d40b307b8dbc1faaa71ea5171e1786e34a19b
rachit <rachitgupta7007@gmail.com>
2020-09-03 02:18:40 +0530

    all operator and its program

---
+++ c_program/3logical_operator.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	                int y,x=5;
+		       	y=x<4||x<10;
+		       	printf("%d\n",y);
+		       	return 0;
+}
---
+++ c_program/conditional_operator.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("Enter two number");
+	scanf("%d%d",&a,&b);
+	printf("Greater number is %d",a>b?a:b);
+	return 0;
+}
