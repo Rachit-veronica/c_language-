9ebfb83cebc52d95cc776c833d21df43002fc8f1
rachit <rachitgupta7007@gmail.com>
2020-10-09 22:29:19 +0530

    when user is delete

---
+++ c_program/string_program/1string.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char i[10];
+
+	int a;
+
+	printf("Enter any name");
+
+	gets(i);
+	a=strlen(i);
+
+	printf("%d\n",a);
+
+	return 0;
+
+}
+
+
+
+
---
+++ c_program/string_program/2string.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+   	char i[100]
+      
+	int a;
+      
+	printf("Enter any name");
+	
+	for(a=0;a<=20;a++)
+
+	gets(i);
+
+	count(0);
+	 return 0;
+
+}
+
+
